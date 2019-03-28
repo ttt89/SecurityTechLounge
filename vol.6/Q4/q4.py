37d8070d7ab7de607d3b878b86a74defced9d11b
ttt89 <8327346+ttt89@users.noreply.github.com>
2019-03-28 17:39:53 +0900

    Create q4.py

---
+++ vol.6/Q4/q4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from ctypes import *
+
+libc = CDLL('libc.so.6')
+
+enter = b'UH\x89\xe5' # push rbp; mov rbp, rsp;
+sc = enter + bytes.fromhex(open('sc').read().replace('\n',''))
+
+c_sc = c_char_p(sc)
+size = len(sc)
+addr = c_void_p(libc.valloc(size))
+memmove(addr, c_sc, size)
+libc.mprotect(addr, size, 0x7)
+cast(addr, CFUNCTYPE(c_void_p))()
+print()
+
