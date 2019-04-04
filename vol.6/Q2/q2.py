7208656ab21bc160fa75b657b3495ddede2867d2
ttt89 <8327346+ttt89@users.noreply.github.com>
2019-04-04 11:20:20 +0900

    Update q2.py

--- vol.6/Q2/q2.py
+++ vol.6/Q2/q2.py
@@ -3,7 +3,7 @@
 from dpkt import *
 from base64 import *
 
-def iter_payload(packets):
+def gen_payload(packets):
   for t,p in packets:
     e = ethernet.Ethernet(p)
     if e.data.p != 17:
@@ -11,7 +11,7 @@ def iter_payload(packets):
     name = dns.DNS(e.data.data.data).qd[0].name
     yield name.split(".")[0]
 
-payload = "".join(iter_payload(pcap.Reader(open("ctf.pcap",'rb'))))
+payload = "".join(gen_payload(pcap.Reader(open("ctf.pcap",'rb'))))
 raw = b32decode(payload)
 
 open("ctf.png", "wb").write(raw)
